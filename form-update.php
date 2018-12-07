a900835c4f404fe154f1da3dbdc766488bb86575
Wes <wesleymilescarr@gmail.com>
2018-12-07 11:26:12 -0500

    Adding site files not within a folder

---
+++ form-update.php
@@ -0,0 +1,80 @@
+
+<?php include('header.php'); ?>
+
+
+	
+	<div class="grid-container">
+		
+    <h2>1. Graphic Design <em>&amp;</em> Usability</h2>
+						<h3>Set in my first job out of college as a graphic designer, below details how I continually enhanced a form which increased the efficiency of our design team.</h3>
+						<div class="border">
+							<img src="images/proof-thumb.png" alt="a form" />
+						</div>
+						<p>The company manufactured security products and printed customer artwork on them. Graphic designers prepared the artwork. A form was attached to every order a designer did. We spent all day in these Adobe Illustrator forms; we knew them well.</p>
+						<p>This form was equally important to two user groups. Every change to the document needed to please both parties. Coordination was in order.</p>
+						<ol>
+							<li><strong>The production team</strong> needed it to confirm the print was correct.</li>
+							<li><strong>And the customer</strong> used it as a final-sign-off for their order.</li>
+						</ol>
+						<a href="#brady" class="more">read more</a>
+					</div>
+
+					<div class="fold-bottom">
+						<div class="three columns">
+							<h4>challenge</h4>
+						</div>
+						<div class="border thirteen columns">
+							<h3>more work, less workers</h3>
+							<p>During the recession, the graphics department downsized workers and increased orders, sameas many other companies around 2008. We went from 20 orders a day to 50. From 5 graphic designers to 2. Those of us remaining were instructed to innovate for efficiency.</p>
+
+							<h3>unhappy customers</h3>
+							<p>Before I worked there, proofs were faxed to customers. Some time later, Brady upgraded to PDF proofs via email. The document displayed a swatch of print, specifications the print shop needed and a rough idea of a final print.</p>
+							<img src="images/proof-version-1.jpg" alt="first iteration of the proof" />
+							<p class="caption">Early verion of the proof page.</p>
+
+							<p>The roughness occasionally caused unhappy customers. In the image below, you can't tell if the text will be facing inwards or out, and you can't tell how far up the lanyard the imprint will be (without measuring). This left some surprised end users when they received their final product. Printers were blamed and customer service took the job of apologizing.</p>
+
+							<h3>new printing facility</h3>
+							<p>The company bought a production facility in China, in addition to the one they had stateside, in 2007. That print shop wasn't two cubes down from graphics anymore, so more specifications were needed on the proofs. With the language barrier and time difference, we needed to be super clear to minimize the risk of mistake. That meant, the proofs now had 3 parties to please; local production, overseas production and customers.</p>
+						</div>
+						<div class="three columns">
+							<h4>process</h4>
+						</div>
+						<div class="border thirteen columns">
+							<h3>apply web knowledge to work</h3>
+							<p>During this time, I was freelancing on the side and inhaling everything on usability I could. It made complete sense to use the principals in '<a href="http://www.sensible.com/dmmt.html" target="blank">Don't make me think</a>' at work. The design had two main user types. For the customers, I designed for an easy scan. We knew they would not check every detail. They, generally, wanted the colors and print to look right in the 3D sample image. For production, I used the "conventions are your friend" axiom. They was familiar to the way the job-ticket looked, so I designed the proof to match that layout. It would be a simpler side-by-side check.</p>
+
+							<h3>talk with customers</h3>
+							<p>To understand how the customer was using these proofs, we chose three high ordering ones to discuss their thoughts of trial proofs. I promoted candidness and 'clear reasons why' in their responses. Having received plenty of design feedback from web projects, I understood how important it is to ask for specific feedback. Bad feedback is subjective. Good feedback gets to the core of why they want this font less bold.</p>
+
+							<h3>testing</h3>
+							<p>The new factory was acquired and the printers needed a proof they would understand, fast. Orders were coming in. I designed different versions and tested them with real jobs. China was able to test our proofs thoroughly, as they received roughly 20 proofs per day. One version had all of the information, but it was too long (two pages total). Another version had different layouts for domestic and overseas production, but that became confusing because some customers received two different looking proofs.</p>
+							<img src="images/proof-version-2.jpg" alt="second iteration of the proof" />
+							<p class="caption">This version fit the data China needed and the realistic imagery the customers wanted.</p>
+
+							<p>Ultimately, I was part of a small team open to change. We didn't have to wait for approval to impliment. The platform we were testing on was perfect for incremental changes. And the high numbers we were working with meant we would hear feedback fast. A perfect environment for tiny tests. As we promoted rock solid communication, the end users and producers let us know how things worked and what could be better.</p>
+						</div>
+
+						<div class="three columns">
+							<h4>solution</h4>
+						</div>
+						<div class="border thirteen columns">
+							<p>Here are the iterations with the final one still used today at the end. We ended up with one sheet, decreased the size of the image and a standardized the angle of the lanyard layout.</p>
+							<img src="images/proof-version-3.jpg" alt="a later iteration of the proof" />
+							<p class="caption">A mid-iteration: limited data for production.</p>
+
+							<img src="images/proof-version-4.jpg" alt="current proof version for a lanyard" />
+							<p class="caption">Current proof for lanyard: data for production made smaller, imprint art and attachments made larger.</p>
+
+							<img src="images/proof-version-4-reel.jpg" alt="current proof version for a badge reel" />
+							<p class="caption">Current proof for badge reel: only pertinent data present.</p>
+						</div>
+
+						<div class="three columns">
+							<h4>results</h4>
+						</div>
+						<div class="border thirteen columns">
+							<p>The amount of calls we got about products being different than the proof decreased. We decided on one proof for both production facilities, which avoided confusion of keeping them separate. The proof set-up time efficiency increased because the lanyard layout was standardized (note the straight angles of the lanyard). One click (an Illustrator action) could be used to transform straight repeated art into a right and left sided lanyard layout.</p>
+						</div>
+
+<?php include('footer.php'); ?>
---
+++ index.php
@@ -0,0 +1,98 @@
+
+<?php include('header.php'); ?>
+
+<body>
+	
+	<div class="grid-container">
+    <div class="grid-x grid-padding-x">
+      <div class="large-12 cell">
+        <h1>Wes is a low-ego designer based in Vermont</h1>
+				<p>He specializes in art direction, collaborating with developers, and balancing what data and his gut tells him.</p>
+      </div>
+
+    </div>
+    <div class="grid-x grid-padding-x">
+      <div class=" cell">
+				<h2>Values</h2>
+			</div>
+			<div class=" cell">
+				<h4>Tie to the business.</h4>
+				<p>I see not just the production tasks, but the entire product ecosystem, from the technology to the business side.</p>
+			</div>
+			<div class=" cell">
+				<h4>Design is a team sport.</h4>
+				<p>I happily join forces with developers and encourage non-designers to give their insight. </p>
+			</div>
+			<div class=" cell">
+				<h4>Don't make (the user) think.</h4>
+				<p>This guiding light prioritizes the user and their entire experience.</p>
+			</div>
+			<div class=" cell">
+				<h4>Data-informed.</h4>
+				<p>Use appropriate research method for the project. Data can help tell the story behind design choices.</p>
+			</div>
+			<div class=" cell">
+				<h4>Code.</h4>
+				<p>Code competency doesn't box in, it shows me the edges of constraint and inspires me to keep up with updates. </p>
+			</div>
+			<div class=" cell">
+				<h4>Explain why.</h4>
+				<p>The hidden side of design is necessary for success. Documentation, proposals, presentations to stakeholders. </p>
+			</div>
+		</div>
+
+<div class="grid-x grid-padding-x">
+      <div class="large-12 cell">
+				<h2>Examples of work</h2>
+			<div class="grid-x grid-padding-x medium-up-2">
+			  <div class="cell">
+			    <div class="card">
+			      <img src="/images/.jpg">
+			      <div class="card-section">
+			      	<h4>iMarket Solutions</h4>
+			        <p>Lead projects which improved website product, designed 150 custom sites optimized for conversion, spearheaded collaboration efforts between dev and design.</p>
+			        <span class="label">Web Design</span>
+			        <span class="label">Product Design</span>
+			        <span class="label">UX</span>
+			      </div>
+			    </div>
+			  </div>
+			  <div class="cell">
+			    <div class="card">
+			      <img src="/images/.jpg">
+			      <div class="card-section">
+			        <h4>Adlife Office</h4>
+			        <p>Helped establish web design arm of a print agency by creating an onboarding/sales process and designing, developing and selling 11 successful sites.</p>
+			        <span class="label">Design</span>
+			      </div>
+			    </div>
+			  </div>
+			  <div class="cell">
+			    <div class="card">
+			      <img src="/images/.jpg">
+			      <div class="card-section">
+			        <h4>Rock & Roll Daycare</h4>
+			        <p>Supported growth of award-winning Boston Montessori School.</p>
+			        <span class="label">Design</span>
+			      </div>
+			    </div>
+			  </div>
+			</div>
+			</div>
+<ol>
+<li><a href=""><strong>Websites for Contractors</strong><br />
+150 Designs over 3 years. Aligned with conversion principles, client expectations, product specs, and branding.</a></li>
+[post detailing my designs, critiques, collaboration, conversion data-informed]
+<li><a href=""><strong>Adlife Office</strong><br />
+Websites and sales systems for a design agency with a storefront</a></li>
+[post about designing systems / business-based design decisions]
+<li><a href=""><strong>Rock & Roll Daycare</strong><br />
+Supported growth of award-winning Boston Montessori School </a></li>
+[post about coding, project management and initiative, and shipping things ]
+<li><a href=""><strong>Refinement of web product</strong><br />
+Spearheading innovation for website product</a></li>
+[CRO project, pattern library, collaboration, mini forms]
+
+</ol>
+
+<?php include('footer.php'); ?>
