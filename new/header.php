a1854d98cbe9c245c891f45821d67dac99296269
Wes <wesleymilescarr@gmail.com>
2018-12-07 11:22:57 -0500

    Uploading new site files

---
+++ new/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/foundation.css">
+		<!-- <link rel='stylesheet' href='style.css' /> -->
+	</head>
+	<body>
